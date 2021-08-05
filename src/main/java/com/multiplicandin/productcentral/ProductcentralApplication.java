25d8ac450c5d90b5aa6036100c0a0d6608b596a3
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2021-08-05 10:01:40 +0530

    initial commit

---
+++ src/main/java/com/multiplicandin/productcentral/ProductcentralApplication.java
@@ -0,0 +1,13 @@
+package com.multiplicandin.productcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiplicandin/productcentral/ProductcentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplicandin.productcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
